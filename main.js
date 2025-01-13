5e01fcace62cf9d6a7439a2ca4bc2d16a6509a8f
ihsanfaisal <ihsanfaisal@gmail.com>
2025-01-13 10:17:07 +0700

    Menambah fungsi ambil daftar pelanggan

--- main.js
+++ main.js
@@ -21,3 +21,22 @@ const firebaseConfig = {
   messagingSenderId: "579109661574",
   appId: "1:579109661574:web:4a7cd4060f70eded945a07"
 };
+
+// fungsi ambil daftar pelanggan
+export async function ambilDaftarPelanggan() {
+  const refDokumen = collection(basisdata, "pelanggan");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikanKueri = await getDocs(kueri);
+
+  let hasilKueri = [];
+  cuplikanKueri.forEach((dokumen) => {
+    hasilKueri.push({
+      id: dokumen.id,
+      nama: dokumen.data().nama,
+      alamat: dokumen.data().alamat,
+      nohape: dokumen.data().nohape
+    })
+  })
+
+  return hasilKueri;
+}
