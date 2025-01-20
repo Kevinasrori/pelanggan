d09c3a619b61e6dcb9de052921644090e38185ba
kevin <Kp8007926@gmail.com>
2025-01-20 10:18:54 +0700

    daftar pelanggan

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js'
-
 import {
   getFirestore,
   collection,
@@ -11,24 +10,24 @@ import {
   updateDoc,
   query,
   orderBy
-} from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
+} from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDFYmmVvk-jLZIeAdYKiTwVw2jqd4VINFA",
-  authDomain: "insan-cemerlang.firebaseapp.com",
-  projectId: "insan-cemerlang",
-  storageBucket: "insan-cemerlang.appspot.com",
-  messagingSenderId: "579109661574",
-  appId: "1:579109661574:web:4a7cd4060f70eded945a07"
+  apiKey: "AIzaSyDBwb2pt0Namd7W8q6fT4tyx1fcY7qX5pQ",
+  authDomain: "pelanggan-1e5c3.firebaseapp.com",
+  projectId: "pelanggan-1e5c3",
+  storageBucket: "pelanggan-1e5c3.firebasestorage.app",
+  messagingSenderId: "253984917517",
+  appId: "1:253984917517:web:8404546b6004a5a0316d44",
+  measurementId: "G-EJQ595DK9F"
 };
 
-//inisialisasi firebase
+// fungsi daftar pelanggan 
 const aplikasi = initializeApp(firebaseConfig)
 const basisdata = getFirestore(aplikasi)
 
-// fungsi ambil daftar pelanggan
 export async function ambilDaftarPelanggan() {
-  const refDokumen = collection(basisdata, "pelanggan");
+  const refDokumen = collection(basisdata, "Pelanggan");
   const kueri = query(refDokumen, orderBy("nama"));
   const cuplikanKueri = await getDocs(kueri);
 
@@ -37,28 +36,50 @@ export async function ambilDaftarPelanggan() {
     hasilKueri.push({
       id: dokumen.id,
       nama: dokumen.data().nama,
-      alamat: dokumen.data().alamat,
-      nohape: dokumen.data().nohape
+      alamat: dokumen.data(). alamat,
+      notelpon: dokumen.data(). notelpon
     })
   })
 
   return hasilKueri;
 }
 
-// fungsi menambah data pelanggan
-export async function tambahPelanggan(nama, alamat, nohape) {
+
+export async function hapuspelanggan(id) {
+  await deleteDoc(doc(basisdata, "barang", id))
+}
+
+
+export async function tambahPelanggan(nama, alamat, notelpon) {
   try {
     // menyimpan data ke firebase
-    const refDokumen = await addDoc(collection(basisdata, "pelanggan"), {
+    const refDokumen = await addDoc(collection(basisdata, "Pelanggan"), {
       nama: nama,
       alamat: alamat,
-      nohape: nohape
+      notelpon: notelpon
     })
 
     // menampilkan pesan berhasil
-    console.log('berhasil menyimpan data pelanggan')
+    console.log("berhasil menyimpan data pelanggan")
   } catch (error) {
-    // menampilkan pesan gagal 
-    console.log('gagal menyimpan data pelanggan')
+    // menampilkan pesan gagal
+    console.log("gagal menyimpan data pelanggan")
   }
+}
+
+export async function hapusPelanggan(id) {
+  await deleteDoc(doc(basisdata, "pelanggan", id))
+}
+export async function ubahPelanggan(id, namabaru, alamatbaru, nohapebaru) {
+  await updateDoc(
+    doc(basisdata, "pelanggan", id),
+    { nama: namabaru, alamat: alamatbaru, notelpon: notelponbaru }
+    )
+}
+
+export async function ambilPelanggan(id) {
+  const refDokumen = await doc(basisdata, "pcoelanggan", id)
+  const snapshotDokumen = await getDoc(refDokumen)
+  
+  return await snapshotDokumen.data()
 }
